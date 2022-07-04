c05499e9a0f112154915012704035cbc7c0af22b
whyusoserious <kegont50@gmail.com>
2022-07-04 23:57:32 +0700

    Created a web-app to checking and adding valide SNILS numbers into db.

---
+++ src/main/java/mainServlet.java
@@ -0,0 +1,51 @@
+
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class mainServlet
+ */
+@WebServlet("/mainServlet")
+public class mainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+//
+//    /**
+//     * Default constructor. 
+//     */
+//    public mainServlet() {
+//        // TODO Auto-generated constructor stub
+//    }
+//
+//	/**
+//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		try {
+			writer.println("<h2>Hello from mainServlet</h2>");
+		}
+		finally {
+			writer.close();
+		}
+	}
+
+}
